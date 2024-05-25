52862dcfac23b6c74bf64d496e0ba1b3315a3cb2
R2D2RDZ <r2d2rdz.secenas@gmail.com>
2024-05-25 02:00:09 -0600

    Robot editor v0.2
    Se puede seleccionar las manos, faltan las demas partes del cuerpo

--- Robot Fghts/Assets/Scripts/RobotCreator.cs
+++ Robot Fghts/Assets/Scripts/RobotCreator.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class RobotCreator : MonoBehaviour
 {
     // Start is called before the first frame update
+    int hand = 0;
+    public GameObject[] Hands = new GameObject[4];
+
     void Start()
     {
         
@@ -13,6 +16,19 @@ public class RobotCreator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.RightArrow)) { ChangeHand(1); }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow)) { ChangeHand(-1); }
     }
+
+    public void ChangeHand(int change)
+    {
+        Hands[hand].SetActive(false);
+        hand += change;
+        if (hand < 0) { hand = Hands.Length - 1; }
+        if (hand >= Hands.Length) { hand = 0; }
+        Hands[hand].SetActive(true);
+
+    }
+
 }
+
