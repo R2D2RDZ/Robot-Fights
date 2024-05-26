90b9cc1788f2ff66ab43403d933ffabd9018b8b5
R2D2RDZ <r2d2rdz.secenas@gmail.com>
2024-05-26 14:01:09 -0600

    Bug fixes 3

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -20,11 +20,32 @@ public class Robot : MonoBehaviour
     [SerializeField] Stats[] BodysStats= new Stats[4];
     [SerializeField] Stats[] LegsStats= new Stats[4];
 
+    private void Start()
+    {
+        if (SceneManager.GetActiveScene().name == "Lobby" && GameObject.FindGameObjectsWithTag("Player").Length > 2)
+        {
+            Destroy(gameObject);
+        }
+    }
+
     private void FixedUpdate()
     {
         //Debug.Log(SceneManager.GetActiveScene().name);
         if (SceneManager.GetActiveScene().name == "MainMenu") Destroy(gameObject);
         DontDestroyOnLoad(this.gameObject);
+        if (SceneManager.GetActiveScene().name == "Lobby" && name=="Robot1")
+        {
+            transform.position = new Vector3(-56.6534615f, -11.8757868f, 51.3726425f);
+            transform.eulerAngles = new Vector3(0, 150, 0);
+            transform.localScale = new Vector3(20.6012573f, 20.6012573f, 20.6012573f);
+        }
+
+        if (SceneManager.GetActiveScene().name == "Lobby" && name == "Robot2")
+        {
+            transform.position = new Vector3(56.6534615f, -11.8757868f, 51.3726425f);
+            transform.eulerAngles = new Vector3(0, 210, 0);
+            transform.localScale = new Vector3(20.6012573f, 20.6012573f, 20.6012573f);
+        }
     }
     public void DisableParts(int[] i)
     {
